9c5d3eda6f3e75dd724ce332d547004e4862eac5
Diego Alvarado <72751822+diegodaac@users.noreply.github.com>
2025-12-15 22:54:18 -0600

    Use environment variables in config.py
    Updated configuration to use environment variables for sensitive data.

--- src/config.py
+++ src/config.py
@@ -1,13 +1,14 @@
-# src/config.py
+import os
+
 class Config:
-    SECRET_KEY = 'PRUEBA'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MYSQL_HOST = '127.0.0.1'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '28040396'
-    MYSQL_DB = 'central_autobuses'
+    MYSQL_HOST = os.getenv('MYSQL_HOST', '127.0.0.1')
+    MYSQL_USER = os.getenv('MYSQL_USER')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+    MYSQL_DB = os.getenv('MYSQL_DB')
 
 config = {
     'development': DevelopmentConfig
